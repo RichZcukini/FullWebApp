a6ccd7415ed67f07c216344be3c6aaaf0fde8676
RichZcukini <sevokzcukini@gmail.com>
2023-07-15 10:33:12 +0300

    First commit

---
+++ src/main/java/FullWebApp/FullWebAppApplication.java
@@ -0,0 +1,13 @@
+package FullWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/FullWebApp/FullWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package FullWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
