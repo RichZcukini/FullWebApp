5e83474342a8687d24d2faff35165f994513c68e
RichZcukini <sevokzcukini@gmail.com>
2023-07-21 12:43:16 +0300

    Fully (?) working create and delete fucntions

--- src/main/java/FullWebApp/EmployeeRepository.java
+++ src/main/java/FullWebApp/EmployeeRepository.java
@@ -1,8 +1,9 @@
 package FullWebApp;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface EmployeeRepository extends PagingAndSortingRepository<Employee, Long>, CrudRepository<Employee, Long> {
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
 }
